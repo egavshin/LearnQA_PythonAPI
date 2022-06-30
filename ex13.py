1e94ca55f3c338002b58950b7bc5eb656466de29
egavshin <osma902010@gmail.com>
2022-06-30 23:03:23 +0300

    Домашнее задание

--- ex13.py
+++ ex13.py
@@ -1,10 +1,12 @@
 import requests
 import pytest
+import pytest_check as check
+
 
 class TestUserAgent:
     agents = [
         ("Mozilla/5.0 (Linux; U; Android 4.0.2; en-us; Galaxy Nexus Build/ICL53F) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30", {'platform': 'Mobile', 'browser': 'No', 'device': 'Android'}),
-        ("Mozilla/5.0 (iPad; CPU OS 13_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/91.0.4472.77 Mobile/15E148 Safari/604.1", {'platform': 'Mobile', 'browser': 'Chrome', 'device': 'iOSX'}),
+        ("Mozilla/5.0 (iPad; CPU OS 13_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/91.0.4472.77 Mobile/15E148 Safari/604.1", {'platform': 'Mobile', 'browser': 'Chrome', 'device': 'iOS'}),
         ("Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)", {'platform': 'Googlebot', 'browser': 'Unknown', 'device': 'Unknown'}),
         ("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Safari/537.36 Edg/91.0.100.0", {'platform': 'Web', 'browser': 'Chrome', 'device': 'No'}),
         ("Mozilla/5.0 (iPad; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1", {'platform': 'Mobile', 'browser': 'No', 'device': 'iPhone'})
@@ -20,6 +22,10 @@ class TestUserAgent:
 
         print(response_dict)
 
-        assert expected_values['platform'] in response_dict['platform'], f"The 'platform' specified incorrectly. Now '{response_dict['platform']}', but should be '{expected_values['platform']}' "
-        assert expected_values['browser'] in response_dict['browser'], f"The 'browser' specified incorrectly. Now '{response_dict['browser']}', but should be '{expected_values['browser']}'"
-        assert expected_values['device'] in response_dict['device'], f"The 'device' specified incorrectly. Now '{response_dict['device']}', but should be '{expected_values['device']}'"
+        check.equal(expected_values['platform'], response_dict['platform']), f"The 'platform' specified incorrectly. Now '{response_dict['platform']}', but should be '{expected_values['platform']}' "
+        check.equal(expected_values['browser'], response_dict['browser']), f"The 'platform' specified incorrectly. Now '{response_dict['browser']}', but should be '{expected_values['browser']}' "
+        check.equal(expected_values['device'], response_dict['device']), f"The 'platform' specified incorrectly. Now '{response_dict['device']}', but should be '{expected_values['device']}' "
+
+       # assert expected_values['platform'] in response_dict['platform'], f"The 'platform' specified incorrectly. Now '{response_dict['platform']}', but should be '{expected_values['platform']}' "
+       # assert expected_values['browser'] in response_dict['browser'], f"The 'browser' specified incorrectly. Now '{response_dict['browser']}', but should be '{expected_values['browser']}'"
+       # assert expected_values['device'] in response_dict['device'], f"The 'device' specified incorrectly. Now '{response_dict['device']}', but should be '{expected_values['device']}'"
